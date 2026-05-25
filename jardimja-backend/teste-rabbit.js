4ae4328d84d7534eeea1595422ae70fd5fadf336
Athoosz <usacrabrasilathos@gmail.com>
2026-05-25 19:13:49 -0300

    update test

--- jardimja-backend/teste-rabbit.js
+++ jardimja-backend/teste-rabbit.js
@@ -2,25 +2,36 @@ require('dotenv').config();
 
 const { publicarEvento } = require('./src/events/publisher');
 const { iniciarConsumidores } = require('./src/events/consumer');
+const aguardar = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function testar() {
   await iniciarConsumidores();
 
-  await publicarEvento('nova_solicitacao', {
-    solicitacaoId: 1,
-    tipo: 'Corte de grama',
-    clienteNome: 'Joao Teste',
-    endereco: 'Rua das Flores, 100'
-  });
+  let solicitacaoId = 1;
 
-  await publicarEvento('solicitacao_aceita', {
-    solicitacaoId: 1,
-    jardineiroNome: 'Carlos Souza'
-  });
+  setInterval(async () => {
+    await publicarEvento('nova_solicitacao', {
+      solicitacaoId,
+      tipo: 'Corte de grama',
+      clienteNome: 'Joao Teste',
+      endereco: 'Rua das Flores, 100'
+    });
 
-  await publicarEvento('servico_concluido', {
-    solicitacaoId: 1
-  });
+    await aguardar(3000);
+
+    await publicarEvento('solicitacao_aceita', {
+      solicitacaoId,
+      jardineiroNome: 'Carlos Souza'
+    });
+
+    await aguardar(3000);
+
+    await publicarEvento('servico_concluido', {
+      solicitacaoId
+    });
+
+    solicitacaoId += 1;
+  }, 20000);
 }
 
 testar();
